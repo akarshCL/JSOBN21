80fea157569336f51ea6ad8ca62448cefec9ae92
akarshCL <akarshg222@gmail.com>
2026-05-13 11:31:18 +0530

    ok

---
+++ forLoopday1.js
@@ -0,0 +1,145 @@
+// // // // Print numbers from 1 to 10 using a for loop.
+
+// // // // Print all even numbers from 1 to 50.
+// // // // Print all odd numbers from 1 to 50.
+// // // // Find the sum of numbers from 1 to 100.
+// // // // Find the product of numbers from 1 to 5.
+// // // // Print the multiplication table of 7.
+// // // // Count how many numbers are divisible by 3 between 1 to 100.
+// // // // Print squares of numbers from 1 to 10.
+// // // // Print cubes of numbers from 1 to 10.
+
+
+
+// // // // Print numbers from 10 to 1.
+
+// // // // Easy-Intermediate Level
+
+// // // // Find the factorial of a number.
+
+
+// // // // Reverse a number using a loop.
+// // // // Count digits in a number.
+// // // // Find the sum of digits of a number.
+// // // // Check whether a number is Armstrong or not.
+// // // // Check whether a number is Prime or not.
+// // // // Find the largest digit in a number.
+// // // // Find the smallest digit in a number.
+// // // // Check whether a number is Palindrome or not.
+
+
+// // // // Intermediate Level
+// // // // Print Fibonacci series up to n terms.
+
+// // // // Check whether a number is Perfect number or not.
+// // // // Print all factors of a number.
+// // // // Count total factors of a number.
+
+
+
+
+
+
+
+
+// // // // let a=10; //11
+
+// // // // console.log(a++,"increment")
+// // // // console.log(a,"increment")
+// // // // let b=10; //11
+// // // // console.log(++b,"increment")
+// // // // console.log(b,"increment")
+// // // // ++b;// 11
+// // // // --b; // 10
+// // // // b++;// 10
+// // // // ++b;// 12
+// // // // b--; //12
+// // // // ++b;//12
+// // // // --b;//11
+// // // // console.log("b",b)
+
+// // // // let n=10;
+// // // // if(n%2==0){
+// // // //  console.log(n++) //10
+// // // //  console.log(n)//11
+// // // // }else{
+// // // //  console.log(--n)
+// // // // }
+// // // // console.log(n,"num") // 11
+
+// // // let a = 999;//1000//999
+// // // if (true) {
+// // //     a++;  // 999
+// // //     if (true) {
+// // //         a--;//1000
+// // //         if (false) {
+// // //             --a;
+// // //         } else {
+// // //             if (true) {
+// // //                 ++a;//1000
+// // //             }
+// // //         }
+// // //     }
+
+// // //     if ("" == " ") {
+// // //         a--;//
+// // //     }
+// // //     if (false) {
+// // //         console.log(++a)
+// // //     } else if (0 == "0") {
+// // //         console.log(a--) // 1000
+// // //     } else {
+// // //         a--;
+// // //     }
+// // // }
+// // // console.log(a) //999
+
+// // // for loop i= 1; i<=10
+// // //  for (initialization; condition; inc/decr/any updation){
+// // // 
+// // // }
+
+
+
+
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // let n=18
+
+// // for( let i =1;i<=10;i++){
+// // console.log(`${n} * ${i} = ${n*i}`)
+// // }
+// // 3 4 5 6 7 8 9 10. // 3+ 6+ 9
+
+// // // Count how many numbers are divisible by 3 between 1 to 100.
+// let count=0;
+// for( let i =3;i<=10;i++){
+//     // if(i%3==0){
+//         count= count+i
+//     // }
+
+// }
+// console.log(count)
+
+
+// for (let i=10;i>0;i--){
+//     console.log(i)
+// }
+
+
+// for (let i=10;i>0;i++){
+//     console.log(i)
+// }
---
+++ ternaryandswitch.js
@@ -0,0 +1,189 @@
+
+// // // // ternary operator:
+// // // if (true) {
+// // //     if (true) {
+// // //         if (true) {
+
+// // //         } else {
+
+// // //         }
+// // //     } else {
+
+// // //     }
+// // // } else {
+
+// // // }
+// // // let n = -10
+
+// // // // condition?true:false
+// // // // console.log(10 == "10" ? true : false)
+// // // // console.log(n > 0 ? "its a positive value" : "its a negative value")
+
+// // // // console.log( n>0 ? ((true) ? "its a zero value" : "its a positive value")  : "its a negative number ")
+
+// // // // console.log( ((n===0) ? "its a zero value" : "its a positive value") )
+
+// // // let marks=75;
+// // // console.log(marks>=90 ?( "its a a grade"):(( marks>=80) ? ("its a b grade "): ("fail")))
+
+
+
+
+
+// // //
+// // let day = 1
+
+// // switch (day) {
+// //     case (1):
+// //         console.log("monday");
+// //         return;
+
+// //     case 2:
+// //         console.log("tuesday")
+// //           break;
+// //     case 3:
+// //         console.log("wed")
+// //           break;
+// //     case 4:
+// //         console.log("thur")
+// //           break;
+// //     case 5:
+// //         console.log("fri")
+// //           break;
+// //     case 6:
+// //         console.log("sat")
+// //           break;
+// //     case 7:
+// //         console.log("sun")
+// //           break;
+
+// //     default:
+// //         console.log("invalid number")
+
+
+// // }
+
+
+
+
+
+
+
+
+// // // let str=" "
+// // // console.log(str == "")
+// // // " "==""
+// // // console.log("","string is emptry")
+// // // console.log(" ","log space")
+// // // console.log("juhygfd","some value");
+
+
+
+// // function abc(){
+// //     console.log("hello")
+// // }
+
+// // abc();
+
+// // function xyz(){
+// //     hjsdgfsh 
+// //     return;
+// // }
+
+// // console.log(xyz())
+
+
+
+
+
+
+
+
+
+
+
+// // // // Write a program using the ternary operator to check whether a number is even or odd.
+// // // // Write a program using the ternary operator to check whether a number is positive or negative.
+// // // // Write a program using the ternary operator to check whether a person is eligible to vote (age >= 18).
+// // // // Write a program using the ternary operator to check whether a student has passed or failed (passing marks = 33).
+// // // // Write a program using the ternary operator to find the greater of two numbers.
+// // // // Write a program using nested ternary operators to find the largest of three numbers.
+// // // // Write a program using the ternary operator to check whether a year is a leap year or not.
+// // // // Write a program using the ternary operator to assign grades:
+// // // // Marks >= 90 → A+
+// // // // Marks >= 75 → A
+// // // // Marks >= 60 → B
+// // // // Otherwise → C
+// // // // Write a program using the ternary operator to check whether a string is empty or not.
+// // // // Write a program using the ternary operator to display "Logged In" or "Please Login" based on a boolean variable.
+// let a = 10;
+// let b = 30;
+// let operator = "/"
+
+// if (operator == "/")
+//     switch (operator) {
+//         case "/":
+//             let data = a / b
+//             break;
+//         case "+":
+//             dfghj
+//             break;
+//         default:
+//             fghjk;
+//     }
+
+
+
+// // // // Write a program using switch to print the day name based on a number (1–7).
+// // // // Write a program using switch to print the month name based on a number (1–12).
+// // // // Write a program using switch to create a simple calculator that supports +, -, *, /, and %.
+
+// // // // Write a program using switch to display traffic light messages:
+// // // // red → Stop
+// // // // yellow → Ready
+// // // // green → Go
+
+
+
+
+
+
+// // // // Write a program using switch to check whether a character is a vowel or consonant.
+// // // // Write a program using switch to display the number of days in a month.
+// // // // Write a program using switch to identify the browser name:
+// // // // Chrome
+// // // // Firefox
+// // // // Safari
+// // // // Edge
+// // // // Write a program using switch to print whether a day is a weekday or weekend.
+// // // // Write a program using switch to display grade remarks:
+// // // // A → Excellent
+// // // // B → Good
+// // // // C → Average
+// // // // D → Needs Improvement
+// // // // Write a program using unary + to convert a string "100" into a number.
+// // // // Write a program using unary - to convert "50" into -50.
+// // // // Write a program demonstrating postfix increment (x++).
+// // // // Write a program demonstrating prefix increment (++x).
+// // // // Write a program demonstrating postfix decrement (x--).
+// // // // Write a program demonstrating prefix decrement (--x).
+// // // // Write a program using ! to reverse a boolean value.
+// // // // Write a program using !! to convert any value into a boolean.
+
+
+
+// let day = 3;
+// switch (day) {
+//     case 7:
+//     case 8:
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log("its a weekday")
+//         break;
+//     case 6:
+//     case 7:
+//         console.log("weekend just enjoy ur day!!!")
+//     default:
+//         console.log("its a invalid day")
+// }
