89da309a8ec53f3e834c6791cefefb43fe5fff77
akarshCL <akarshg222@gmail.com>
2026-05-18 16:55:48 +0530

    ok

---
+++ function.js
@@ -0,0 +1,166 @@
+// // // console.log('function')
+
+
+
+
+
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+// // // console.log("hello")
+
+// // // console.log("hello Aka")
+// // // console.log("hello na")
+// // // console.log("hello hd")
+// // // console.log("hello gd")
+// // // console.log("hello euy")
+// // // console.log("hello ueu")
+// // // let a=10;
+// // a=90
+// // a=80
+
+// // function welcome(){
+// //        let a="10";
+// // console.log("hello ueu1",a)
+// // }
+// // function welcome(){
+// //     let b=10;
+// // console.log("hello ueu12",b)
+// // }
+// // function welcome(){
+// //        let c=true;
+// // console.log("hello ue56u",c)
+// // }
+
+
+// // console.log(welcome());// function call
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // // Write a function to check palindrome number.
+// // // // Write a function to reverse a number.
+// // // // Write a function to count digits in a number.
+// // // // Write a function to find factorial.
+
+// // // // Write a function to print Fibonacci series.
+// // // // Write a function to calculate power without built-in method.
+// // // // Write a function to count occurrence of a digit in a number.
+
+// // // Create a function and call it.
+// // // Write a function welcome() and invoke it 3 times.
+
+// // // What is function invocation?
+// // // Predict the output:
+// // // function hello() {
+// // //   console.log("Hello");
+// // // }
+
+// // // hello;
+// // // Intermediate
+// // // Write a function to print Fibonacci series and call it with different values.
+// // // Create a function that prints multiplication table and invoke it for:
+// // // 2
+// // // 5
+// // // 10
+// // // Write a function checkLeapYear(year) and call it with 2024 and 2025.
+// // // Create a calculator function:
+// // // calculator(a, b, operation)
+
+// // // and invoke it for:
+
+// // // addition
+// // // subtraction
+// // // multiplication
+// // // Write a function that accepts a number and invokes another function to check whether it is prime.
+
+
+
+// let n=1;
+// let n2=4
+
+// // function welcome(a,b){
+// //     // console.log(`hello ${name}`)
+// //     console.log(a,"a",b,"b")
+// //     console.log(a+b)
+// //     a-b
+// // }
+
+
+// // welcome(n,n2) //
+// // welcome(3,6)
+// // welcome(true,false)
+// // welcome("ykutd","jhgxc")
+
+// function abc(a,b){
+// console.log(a+b+c+d+e+f)
+// }
+
+// abc(1,100)
+
+
+// // // // Write a function to count digits in a number.
+
+// function count(n){
+//     console.log(n)
+
+
+// }
+
+// count(12345)
+
+
+// let count=function(){
+    
+// }
+
+// count();
+function value(name,age){
+    
+    return   "hello",12,true;
+   
+}
+
+console.log(value())
--- whileloop.js
+++ whileloop.js
@@ -4,34 +4,34 @@
 
 
 
-// // let a=10;
-// // if(a==10){
-// //   a=13;
-// //   console.log(a)
+// // // let a=10;
+// // // if(a==10){
+// // //   a=13;
+// // //   console.log(a)
+// // // }
+// // // console.log(a)
+
+// // let a=0;
+// // let b=1;
+// // let n=10;
+// // for (let i= 2;i<=n;i++){
+// //     console.log(a)
+// //   let c=a+b;
+
+// // a=b;
+// //  b=c;
+
+// //  console.log(c,"c")
 // // }
-// // console.log(a)
-
-// let a=0;
-// let b=1;
-// let n=10;
-// for (let i= 2;i<=n;i++){
-//     console.log(a)
-//   let c=a+b;
-
-// a=b;
-//  b=c;
-
-//  console.log(c,"c")
-// }
 
 
 
 
-// let n=-11;
-// while(n<=-10){
-// console.log("hello")
-// n--; // decrease
-// }
+// // let n=-11;
+// // while(n<=-10){
+// // console.log("hello")
+// // n--; // decrease
+// // }
  
 
 
@@ -45,11 +45,11 @@
 
 
 
-// Simple Rule to Remember
-// while → "Check first, then run."
-// do...while → "Run first, then check."
-// break → "Stop now."
-// continue → "Skip this one."
+// // Simple Rule to Remember
+// // while → "Check first, then run."
+// // do...while → "Run first, then check."
+// // break → "Stop now."
+// // continue → "Skip this one."
 
 
 
@@ -59,14 +59,14 @@
 
 
 
-// Practice Questions
+// // Practice Questions
 
-// While Loop
-// Print numbers from 1 to 10.
-// Print numbers from 10 to 1.
-// Print even numbers from 1 to 20.
-// Find sum of numbers from 1 to 100.
-// Print multiplication table of 7.
+// // While Loop
+// // Print numbers from 1 to 10.
+// // Print numbers from 10 to 1.
+// // Print even numbers from 1 to 20.
+// // Find sum of numbers from 1 to 100.
+// // Print multiplication table of 7.
 
 
 
@@ -78,18 +78,18 @@
 
 
 
-// Do While Loop
-// Print numbers from 1 to 5 using do...while.
-let n=-1;
-do{
-console.log(n);
-n--;
-}while(n>0);
+// // Do While Loop
+// // Print numbers from 1 to 5 using do...while.
+// let n=-1;
+// do{
+// console.log(n);
+// n--;
+// }while(n>0);
 
-// Print odd numbers from 1 to 15.
-// Print table of 9.
-// Find factorial of 5.
-// Print digits of a number.
+// // Print odd numbers from 1 to 15.
+// // Print table of 9.
+// // Find factorial of 5.
+// // Print digits of a number.
 
 
 
@@ -110,45 +110,76 @@ n--;
 
 
 
-// Break
-// Print numbers from 1 to 20, stop at 12.
-// Find first number divisible by 17 between 1 and 100.
-// Search for number 25 in an array.
-// Stop loop when sum exceeds 50.
-// Ask user for password until correct.
+// // Break
+// // Print numbers from 1 to 20, stop at 12.
 
 
 
+// // Ask user for password until correct.
 
 
 
 
 
 
+// // Stop loop when sum exceeds 50. 1-20
+// console.log(sum)
+// if(sum>=50){
+//     break;
+// }
 
 
+// // Skip numbers divisible by 3 in between 1-20
 
 
 
+// // Find first number divisible by 17 between 1 and 100.
+// // Print all numbers except even numbers.
 
 
 
 
 
 
-// Continue
-// Print numbers from 1 to 10, skip 5.
-// Print only odd numbers from 1 to 20.
-// Skip numbers divisible by 3.
-// Print array values except null.
-// Print all numbers except even numbers.
 
 
 
 
 
 
+// // Continue
+// // Print numbers from 1 to 10, skip 5.
+// // Print only odd numbers from 1 to 20.
 
+// // Print array values except null.
+
+
+
+
+// for (let i=0;i<=10;i++){
+    
+//     if(i==5){
+//         // break;
+//         // continue;
+//           console.log(i)
+        
+//     }
+  
+// }
+// //   console.log(i)
+
+
+
+for (let i=0;i<=10;i++){
+     console.log(i);
+    if(i==5){
+        break;
+        // continue;
+         
+        
+    }
+  
+}
 
 
 
