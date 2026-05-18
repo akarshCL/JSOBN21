753bba56634d5f68acb63e50f501c133f3bfe088
akarshCL <akarshg222@gmail.com>
2026-05-18 10:16:04 +0530

    ok

--- forLoopday1.js
+++ forLoopday1.js
@@ -1,145 +1,271 @@
-// // // // Print numbers from 1 to 10 using a for loop.
+// // // // // // // Print numbers from 1 to 10 using a for loop.
 
-// // // // Print all even numbers from 1 to 50.
-// // // // Print all odd numbers from 1 to 50.
-// // // // Find the sum of numbers from 1 to 100.
-// // // // Find the product of numbers from 1 to 5.
-// // // // Print the multiplication table of 7.
-// // // // Count how many numbers are divisible by 3 between 1 to 100.
-// // // // Print squares of numbers from 1 to 10.
-// // // // Print cubes of numbers from 1 to 10.
-
-
-
-// // // // Print numbers from 10 to 1.
-
-// // // // Easy-Intermediate Level
-
-// // // // Find the factorial of a number.
-
-
-// // // // Reverse a number using a loop.
-// // // // Count digits in a number.
-// // // // Find the sum of digits of a number.
-// // // // Check whether a number is Armstrong or not.
-// // // // Check whether a number is Prime or not.
-// // // // Find the largest digit in a number.
-// // // // Find the smallest digit in a number.
-// // // // Check whether a number is Palindrome or not.
-
-
-// // // // Intermediate Level
-// // // // Print Fibonacci series up to n terms.
-
-// // // // Check whether a number is Perfect number or not.
-// // // // Print all factors of a number.
-// // // // Count total factors of a number.
+// // // // // // // Print all even numbers from 1 to 50.
+// // // // // // // Print all odd numbers from 1 to 50.
+// // // // // // // Find the sum of numbers from 1 to 100.
+// // // // // // // Find the product of numbers from 1 to 5.
+// // // // // // // Print the multiplication table of 7.
+// // // // // // // Count how many numbers are divisible by 3 between 1 to 100.
+// // // // // // // Print squares of numbers from 1 to 10.
+// // // // // // // Print cubes of numbers from 1 to 10.
 
 
 
+// // // // // // // Print numbers from 10 to 1.
+// // // // let str="1"
+// // // // str=str+"2"
+// // // // console.log(str,"str")
+// // // let res;//10
+// // // for(let i=10; i>=1;i -= 1){
+// // //     res = res +""+ i
+// // //  console.log(res,"everystep")
+// // // }
+// // //    console.log(res)
+// // // // // // // Easy-Intermediate Level
+
+// // // // // // // Find the factorial of a number.
+
+
+// // // // // // // Reverse a number using a loop.
+// // // // // // // Count digits in a number.
+// // // // // // // Find the sum of digits of a number.
+// // // // // // // Check whether a number is Armstrong or not.
+// // // // // // // Check whether a number is Prime or not.
+// // // // // // // Find the largest digit in a number.
+// // // // // // // Find the smallest digit in a number.
+// // // // // // // Check whether a number is Palindrome or not.
+
+
+// // // // // // // Intermediate Level
+// // // // // // // Print Fibonacci series up to n terms.
+
+// // // // // // // Check whether a number is Perfect number or not.
+// // // // // // // Print all factors of a number.
+// // // // // // // Count total factors of a number.
 
 
 
 
 
-// // // // let a=10; //11
 
-// // // // console.log(a++,"increment")
-// // // // console.log(a,"increment")
-// // // // let b=10; //11
-// // // // console.log(++b,"increment")
-// // // // console.log(b,"increment")
-// // // // ++b;// 11
-// // // // --b; // 10
-// // // // b++;// 10
-// // // // ++b;// 12
-// // // // b--; //12
-// // // // ++b;//12
-// // // // --b;//11
-// // // // console.log("b",b)
 
-// // // // let n=10;
-// // // // if(n%2==0){
-// // // //  console.log(n++) //10
-// // // //  console.log(n)//11
-// // // // }else{
-// // // //  console.log(--n)
+
+// // // // // // // let a=10; //11
+
+// // // // // // // console.log(a++,"increment")
+// // // // // // // console.log(a,"increment")
+// // // // // // // let b=10; //11
+// // // // // // // console.log(++b,"increment")
+// // // // // // // console.log(b,"increment")
+// // // // // // // ++b;// 11
+// // // // // // // --b; // 10
+// // // // // // // b++;// 10
+// // // // // // // ++b;// 12
+// // // // // // // b--; //12
+// // // // // // // ++b;//12
+// // // // // // // --b;//11
+// // // // // // // console.log("b",b)
+
+// // // // // // // let n=10;
+// // // // // // // if(n%2==0){
+// // // // // // //  console.log(n++) //10
+// // // // // // //  console.log(n)//11
+// // // // // // // }else{
+// // // // // // //  console.log(--n)
+// // // // // // // }
+// // // // // // // console.log(n,"num") // 11
+
+// // // // // // let a = 999;//1000//999
+// // // // // // if (true) {
+// // // // // //     a++;  // 999
+// // // // // //     if (true) {
+// // // // // //         a--;//1000
+// // // // // //         if (false) {
+// // // // // //             --a;
+// // // // // //         } else {
+// // // // // //             if (true) {
+// // // // // //                 ++a;//1000
+// // // // // //             }
+// // // // // //         }
+// // // // // //     }
+
+// // // // // //     if ("" == " ") {
+// // // // // //         a--;//
+// // // // // //     }
+// // // // // //     if (false) {
+// // // // // //         console.log(++a)
+// // // // // //     } else if (0 == "0") {
+// // // // // //         console.log(a--) // 1000
+// // // // // //     } else {
+// // // // // //         a--;
+// // // // // //     }
+// // // // // // }
+// // // // // // console.log(a) //999
+
+// // // // // // for loop i= 1; i<=10
+// // // // // //  for (initialization; condition; inc/decr/any updation){
+// // // // // // 
+// // // // // // }
+
+
+
+
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // // console.log("hello")
+// // // // // let n=18
+
+// // // // // for( let i =1;i<=10;i++){
+// // // // // console.log(`${n} * ${i} = ${n*i}`)
+// // // // // }
+// // // // // 3 4 5 6 7 8 9 10. // 3+ 6+ 9
+
+// // // // // // Count how many numbers are divisible by 3 between 1 to 100.
+// // // // let count=0;
+// // // // for( let i =3;i<=10;i++){
+// // // //     // if(i%3==0){
+// // // //         count= count+i
+// // // //     // }
+
 // // // // }
-// // // // console.log(n,"num") // 11
+// // // // console.log(count)
 
-// // // let a = 999;//1000//999
-// // // if (true) {
-// // //     a++;  // 999
-// // //     if (true) {
-// // //         a--;//1000
-// // //         if (false) {
-// // //             --a;
-// // //         } else {
-// // //             if (true) {
-// // //                 ++a;//1000
-// // //             }
-// // //         }
-// // //     }
 
-// // //     if ("" == " ") {
-// // //         a--;//
-// // //     }
-// // //     if (false) {
-// // //         console.log(++a)
-// // //     } else if (0 == "0") {
-// // //         console.log(a--) // 1000
-// // //     } else {
-// // //         a--;
-// // //     }
+// // // // for (let i=10;i>0;i--){
+// // // //     console.log(i)
+// // // // }
+
+
+// // // // for (let i=10;i>0;i++){
+// // // //     console.log(i)
+// // // // }
+
+
+
+
+// // // // // // // Find the factorial of a number.
+// // //     let fact=1;
+// // // for (let i =1;i<=5;i++){
+
+// // // fact *= i
 // // // }
-// // // console.log(a) //999
-
-// // // for loop i= 1; i<=10
-// // //  for (initialization; condition; inc/decr/any updation){
-// // // 
+// // // console.log(fact)
+// // // // // // // Reverse a number using a loop.
+// // // let n=123;
+// // // let result=0;
+// // // for (let i =n;i>0;i=Math.floor(i/10)){
+// // //    result = result*10+i%10
 // // // }
+// // // console.log(result)
+
+// // // // // // // Count digits in a number.
+
+// // // // // // // Find the sum of digits of a number.
+
+// // // // // // // Check whether a number is Armstrong or not.
+// let number=153;
+// // 1 ka cube + 5 cobe +3 cube==153
 
 
+// // // // // // // Check whether a number is Prime or not.
+
+// // // // // // // Find the largest digit in a number.
+
+// // // // // // // Find the smallest digit in a number.
+
+// // // // // // // Check whether a number is Palindrome or not.
 
 
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // // console.log("hello")
-// // let n=18
+// // // // // // // Intermediate Level
+// // // // // // // Print Fibonacci series up to n terms.
+// // // // // // // Check whether a number is Perfect number or not.
+// // // // // // // Print all factors of a number.
+// // // // // // // Count total factors of a number.
 
-// // for( let i =1;i<=10;i++){
-// // console.log(`${n} * ${i} = ${n*i}`)
+// // let n=10
+// // let res=1;
+// // for(let i=1;i<=n;i++){
+// // res=res*i
 // // }
-// // 3 4 5 6 7 8 9 10. // 3+ 6+ 9
+// // console.log(res)
+// // // // // // Reverse a number using a loop.
 
-// // // Count how many numbers are divisible by 3 between 1 to 100.
-// let count=0;
-// for( let i =3;i<=10;i++){
-//     // if(i%3==0){
-//         count= count+i
-//     // }
-
+// let num=123;
+// let rev=0;
+// for(let i=num; i>0;i= Math.floor(i/10)){
+// let rem=i%10; //3 //2//1
+// // rev=rev*10 +rem //0*10+3=3// 30+2=32//320+1==321
+// if(rev<rem){
+//     rev=rem;
 // }
-// console.log(count)
-
-
-// for (let i=10;i>0;i--){
-//     console.log(i)
 // }
 
+// console.log(rev)
 
-// for (let i=10;i>0;i++){
-//     console.log(i)
+// if(num==rev){
+//     console.log("its a palidrome")
+// }else{
+//       console.log("its not a  palidrome")  
 // }
+// // let num2=123; 87654321
+
+// // for(let i=num2;i>0;i++){
+
+// // let x=(num2%10)
+// // num2=Math.floor(num2/10)
+// // console.log("i2",i);
+// // }
+
+
+
+
+ let n=23;
+// let isPrime=false;
+let count=0;
+
+if(n<=1){
+   count=0;
+}else{
+    for(let i=2;i<n;i++){
+        if(n%i==0){
+           count++
+        }
+        
+    }
+}
+if(coun){
+    console.log("yes")
+}else{
+     console.log("no")
+}
+
+
+
+
+
+
+// let n=10;
+
+
+// let a=0;
+// let b=1
+// let temp=a+b;
+// a=b;
+// b=temp
+
+
+
+
+
---
+++ whileloop.js
@@ -0,0 +1,160 @@
+
+
+
+
+
+
+// // let a=10;
+// // if(a==10){
+// //   a=13;
+// //   console.log(a)
+// // }
+// // console.log(a)
+
+// let a=0;
+// let b=1;
+// let n=10;
+// for (let i= 2;i<=n;i++){
+//     console.log(a)
+//   let c=a+b;
+
+// a=b;
+//  b=c;
+
+//  console.log(c,"c")
+// }
+
+
+
+
+// let n=-11;
+// while(n<=-10){
+// console.log("hello")
+// n--; // decrease
+// }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+// Simple Rule to Remember
+// while → "Check first, then run."
+// do...while → "Run first, then check."
+// break → "Stop now."
+// continue → "Skip this one."
+
+
+
+
+
+
+
+
+
+// Practice Questions
+
+// While Loop
+// Print numbers from 1 to 10.
+// Print numbers from 10 to 1.
+// Print even numbers from 1 to 20.
+// Find sum of numbers from 1 to 100.
+// Print multiplication table of 7.
+
+
+
+
+
+
+
+
+
+
+
+// Do While Loop
+// Print numbers from 1 to 5 using do...while.
+let n=-1;
+do{
+console.log(n);
+n--;
+}while(n>0);
+
+// Print odd numbers from 1 to 15.
+// Print table of 9.
+// Find factorial of 5.
+// Print digits of a number.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Break
+// Print numbers from 1 to 20, stop at 12.
+// Find first number divisible by 17 between 1 and 100.
+// Search for number 25 in an array.
+// Stop loop when sum exceeds 50.
+// Ask user for password until correct.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Continue
+// Print numbers from 1 to 10, skip 5.
+// Print only odd numbers from 1 to 20.
+// Skip numbers divisible by 3.
+// Print array values except null.
+// Print all numbers except even numbers.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
